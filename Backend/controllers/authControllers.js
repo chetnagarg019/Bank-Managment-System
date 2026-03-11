80a58dcbce7cc5f34aaceb1fb1c8e4a5a5c5cef6
chetna <chetnagarg019@gmail.com>
2026-03-11 14:15:52 +0530

    commit #9

--- Backend/controllers/authControllers.js
+++ Backend/controllers/authControllers.js
@@ -1,18 +1,18 @@
 import User from "../model/user.js";
 import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+import emailService from "../services/emailService.js";
 
 async function registerUser(req, res) {
   try {
     const { name, email, password } = req.body;
 
-    const existingUser = await User.findOne({ email : email });
+    const existingUser = await User.findOne({ email: email });
 
     if (existingUser) {
       return res.status(422).json({
         message: "Email already exists",
-        status : "failed"
-        });
+        status: "failed",
+      });
     }
 
     // const hash = await bcrypt.hash(password, 10);
@@ -24,21 +24,18 @@ async function registerUser(req, res) {
     });
 
     //create token
-    const token = jwt.sign( // ye 3 chize leta hai 
-    { id: user._id,}, //Payload (Token ke andar kya store hoga)
-    process.env.JWT_SECRET, //Ye secret key hoti hai jo token ko sign karti hai. Server jab token banata hai to secret key use karta hai.
-    { expiresIn: "7d" } //Matlab token 7 din me expire ho jayega.
-  );
+    const token = jwt.sign(
+      // ye 3 chize leta hai
+      { id: user._id }, //Payload (Token ke andar kya store hoga)
+      process.env.JWT_SECRET, //Ye secret key hoti hai jo token ko sign karti hai. Server jab token banata hai to secret key use karta hai.
+      { expiresIn: "7d" }, //Matlab token 7 din me expire ho jayega.
+    );
 
-  //token save in cookie parser
-  res.cookie("token",token,{
-    httpOnly : true,
-    secure : false
-  });
-
-  
-
-
+    //token save in cookie parser
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+    });
 
     res.status(200).json({
       message: "signup Succesfully",
@@ -47,70 +44,67 @@ async function registerUser(req, res) {
         name: user.name,
         email: user.email,
       },
-      token
+      token,
     });
   } catch (error) {
     console.log(error);
-    
+
     res.status(500).json({ message: "Server error" });
   }
+
+  await emailService.sendRegistrationEmail(User.email,User.name)
 }
 //-------------------------
 
-async function loginUser(req,res){
+async function loginUser(req, res) {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email }).select("+password");
 
-try { const {email,password} = req.body;
-  const user = await User.findOne({email}).select("+password");
+    if (!user) {
+      return res.status(401).json({
+        message: "Invalid Crenditals",
+        status: "failed",
+      });
+    }
 
-  if(!user){
-    return res.status(401).json({
-      message : "Invalid Crenditals",
-      status : "failed"
+    //password check
+    const valid = await user.comparePassword(password); //comparePassword method model/user.js me hai
+
+    if (!valid) {
+      return res.status(401).json({
+        message: "Invalid Crenditals",
+        status: "failed",
+      });
+    }
+
+    //if email and password valid  genrate token
+    const token = jwt.sign(
+      // ye 3 chize leta hai
+      { id: user._id }, //Payload (Token ke andar kya store hoga)
+      process.env.JWT_SECRET, //Ye secret key hoti hai jo token ko sign karti hai. Server jab token banata hai to secret key use karta hai.
+      { expiresIn: "7d" }, //Matlab token 7 din me expire ho jayega.
+    );
+
+    //token save in cookie parser
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
     });
-  }
 
-  //password check
-  const valid = await user.comparePassword(password) //comparePassword method model/user.js me hai 
-
-  if(!valid){
-     return res.status(401).json({
-      message : "Invalid Crenditals",
-      status : "failed"
+    res.status(200).json({
+      user: {
+        _id: user._id,
+        email: user.email,
+        name: user.name,
+      },
+      token,
     });
-  }
-
-  //if email and password valid  genrate token
-   const token = jwt.sign( // ye 3 chize leta hai 
-    { id: user._id,}, //Payload (Token ke andar kya store hoga)
-    process.env.JWT_SECRET, //Ye secret key hoti hai jo token ko sign karti hai. Server jab token banata hai to secret key use karta hai.
-    { expiresIn: "7d" } //Matlab token 7 din me expire ho jayega.
-  );
-
-   //token save in cookie parser
-  res.cookie("token",token,{
-    httpOnly : true,
-    secure : false
-  });
-
-  res.status(200).json({
-    user : {
-      _id : user._id,
-      email : user.email,
-      name : user.name
-    },
-    token
-  });
-}catch (error) {
+  } catch (error) {
     console.log(error);
-    
+
     res.status(500).json({ message: "Server error" });
   }
-
 }
 
-
-
-
-
-
-export default { registerUser,loginUser };
+export default { registerUser, loginUser };
