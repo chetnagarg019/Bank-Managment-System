910c3db0b6b07a1c3627679236b8307df2676bca
chetna <chetnagarg019@gmail.com>
2026-04-03 23:55:21 +0530

    commit #23

--- Backend/controllers/transactionController.js
+++ Backend/controllers/transactionController.js
@@ -39,36 +39,44 @@ async function createTransaction(req,res) {
 
     if(isTransactionAlreadyExists){
         if(isTransactionAlreadyExists.status === "Completed"){
-            res.status(200).json({
+            return res.status(200).json({
                 message : "Transaction already processed",
                 transation : isTransactionAlreadyExists
             })
         }
 
         if(isTransactionAlreadyExists.status === "Pending"){
-            res.status(200).json({
+            return res.status(200).json({
                 message : "Transaction is still processing",
             })
         }
 
         if(isTransactionAlreadyExists.status === "Failed"){
-            res.status(400).json({
+            return res.status(400).json({
                 message : "Transaction Failed",
             })
         }
 
           if(isTransactionAlreadyExists.status === "Reversed"){
-            res.status(500).json({
+            return res.status(500).json({
                 message : "Transaction was reversed,try again",
             })
         }
 
     }
 
-    // 
+    //check account status
+    if(fromUserAccount.status !== "Active" || toUserAccount !== "Active"){
+        return res.status(400).json({
+            message : "both from account and to account must be active to process transaction"
+        })
+
+    } 
+
+    //4.drive sender ledger check krna hai sender ke paas utna balance hai bhi ya nhii 
 
     
-
+ 
 
 
 
@@ -78,3 +86,15 @@ async function createTransaction(req,res) {
 
 
 }
+
+//10 steps
+//1.validate request
+//2.valid idempotency key
+//3.check account status
+//4.drive sender balance from ledger
+//5.create transaction(pending)
+//6. create debit ledger entry 
+//7. create credit ledger entry
+//8.mark transaction complete
+//9.commit mongoDb session
+//10.send email notifaction
